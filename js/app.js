55af4c367ae8479e218234405e05a9e2996e05ca
Louis <loveofsunny1345@gmail.com>
2019-07-25 10:27:31 +0700

    ogami

---
+++ js/app.js
@@ -0,0 +1,16 @@
+window.addEventListener('scroll', function(ev) {
+
+    var navigation = document.getElementById('navigation');
+    var sticky = navigation.offsetTop;
+    // var distanceToTop = navigation.getBoundingClientRect().top;
+
+    if(window.pageYOffset > sticky){
+        navigation.classList.add('nav-animation-active');
+        navigation.classList.remove('nav-animation-deactive');
+    }
+    else {
+        navigation.classList.remove('nav-animation-active');
+        navigation.classList.add('nav-animation-deactive');
+
+    }
+});
